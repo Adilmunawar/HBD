15fb4810dc5cf0586a9c2ec5dd03b9f7fe4718ea
Adil Munawar <adilfromavengers@gmail.com>
2025-09-13 20:40:13 +0000

    this ai animation idea also

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,9 @@ export default function Home() {
     <div className="relative min-h-screen w-full overflow-x-hidden">
       <Balloons />
       <main className="relative z-10 flex flex-col items-center justify-center w-full px-4 py-8 sm:px-6 lg:px-8 min-h-screen">
-        <div className="w-full max-w-6xl mx-auto">
-          <header className="text-center my-8 md:my-16 flex flex-col items-center">
-            <NameAnimation />
-            <BirthdayBanner />
-          </header>
+        <div className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center gap-12">
+          <NameAnimation />
+          <BirthdayBanner />
         </div>
       </main>
     </div>
