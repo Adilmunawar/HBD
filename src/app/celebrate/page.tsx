aaee2f3d39dbe31b526c5590c899c3b1f18e2726
Adil Munawar <adilfromavengers@gmail.com>
2025-09-13 21:08:10 +0000

    next we will add this sticker on the second page and we will write "Arry

--- src/app/celebrate/page.tsx
+++ src/app/celebrate/page.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+
 export default function CelebratePage() {
   return (
     <div className="relative min-h-screen w-full overflow-x-hidden">
       <main className="relative z-10 flex flex-col items-center justify-center w-full px-4 py-8 sm:px-6 lg:px-8 min-h-screen">
-        {/* We can add exciting things here! */}
+        <Image
+          src="/cute-bunny.gif"
+          alt="Cute animated bunny"
+          width={380}
+          height={380}
+          unoptimized
+        />
+        <p className="mt-4 text-2xl font-bold text-foreground">
+          Arry Itaa piyarra insan
+        </p>
       </main>
     </div>
   );
