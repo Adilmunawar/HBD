1f443bad165dc099ea50254681db00f9f0bc03d8
Adil Munawar <adilfromavengers@gmail.com>
2025-09-28 17:47:22 +0000

    I'm facing this glitchy issue with all buttons of the pages

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -47,9 +47,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
-      <Spotlight className={cn(buttonVariants({ variant: 'outline', size, className: 'p-0 border-none' }))}>
+      <Spotlight className={cn(buttonVariants({ variant: 'default', size, className: 'p-0 border-none' }), className)}>
         <Comp
-          className={cn(buttonVariants({ variant, size, className }), 'w-full h-full')}
+          className={cn(buttonVariants({ variant, size, className }), 'w-full h-full bg-transparent')}
           ref={ref}
           {...props}
         />
